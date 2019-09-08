3ac869714f0f8d8417c864b841edf7032a3ebbbf
Thomas Herring <teh6@rice.edu>
2019-09-08 03:02:13 -0500

    Uncommented out actions

--- install.sh
+++ install.sh
@@ -23,25 +23,26 @@ done
 
 # Add nerdfont install
 echo 'Installing Nerdfonts...'
-#git clone https://github.com/ryanoasis/nerd-fonts.git
-#./nerd-fonts/install.sh
-#rm -rf nerd-fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git
+./nerd-fonts/install.sh
+rm -rf nerd-fonts
 
 
 # Install Vundle and install plugins
 echo 'Installing Vundle and VIM plugins...'
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#rm -f ~/.vimrc
-#ln -s $PWD/vimrc ~/.vimrc
-#vim +PluginInstall +qall
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm -f ~/.vimrc
+ln -s $PWD/vimrc ~/.vimrc
+vim +PluginInstall +qall
 
 # Install tmux params
-echo 'Installing Tmux'
-#git clone https://github.com/samoshkin/tmux-config.git
-#./tmux-config/install.sh
-#rm -rf tmux-config
-#rm -f ~/.tmux.conf
-#ln -s $PWD/tmux.conf ~/.tmux.conf
+echo 'Installing Tmux...'
+git clone https://github.com/samoshkin/tmux-config.git
+./tmux-config/install.sh
+rm -rf tmux-config
+rm -f ~/.tmux.conf
+ln -s $PWD/tmux.conf ~/.tmux.conf
 
 # Add bashrc source
+echo 'Installing commands...'
 echo 'source '$PWD'/setup.bash' >> ~/.bashrc
