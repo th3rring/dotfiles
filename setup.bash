581ecba4ea191e0e140840378a1305c11cb53dcc
Thomas Herring <teh6@rice.edu>
2019-09-08 03:25:34 -0500

    Added proper ssh and dmux execute

--- setup.bash
+++ setup.bash
@@ -2,9 +2,10 @@
 install_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 krl_machine='megamedes'
-alias devssh='sshpass -p "Qrioqrio9" ssh -o ScriptHostKeyChecking=no teh6@'$krl_machine'.cs.rice.edu'
+alias devssh='sshpass -p "Qrioqrio9" ssh -o StrictHostKeyChecking=no therring@'$krl_machine'.cs.rice.edu'
 
 alias dmux=$install_path'/dev-tmux.sh'
 
 work_path='Workspace/xe2ez'
 alias devcd='cd ~/'$work_path
+alias vpn=/opt/cisco/anyconnect/bin/vpn
