61512e35bb1aea7f81a93c4b578e0e5effc2c4a4
Thomas Herring <teh6@rice.edu>
2019-09-08 03:21:57 -0500

    Added VPN to installer

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ function install_progs {
 	echo "I'm installing programs"
 	if [ $USER != 'root' ]
 	then
-		echo 'Not running as sudo, please authenticate.'
+		echo 'Not running as root, please authenticate.'
 	fi
 	sudo apt install $programs
 }
@@ -27,7 +27,6 @@ git clone https://github.com/ryanoasis/nerd-fonts.git
 ./nerd-fonts/install.sh
 rm -rf nerd-fonts
 
-
 # Install Vundle and install plugins
 echo 'Installing Vundle and VIM plugins...'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -46,3 +45,25 @@ ln -s $PWD/tmux.conf ~/.tmux.conf
 # Add bashrc source
 echo 'Installing commands...'
 echo 'source '$PWD'/setup.bash' >> ~/.bashrc
+
+# Ask to install vpn
+function install_vpn {
+	tar -xvf anyconnect.tar.gz
+	if [ $USER != 'root' ]
+	then
+		echo 'Not running as root, please authenticate.'
+	fi
+	sudo ./anyconnect-linux64-4.7.00136/vpn/vpn_install.sh
+	echo 'alias vpn='/opt/cisco/anyconnect/bin/vpn'' >> setup.bash
+	rm -rf anyconnect-linux64-4.7.00136
+}
+while true; do
+    read -p "Would you like to install Rice vpn?" yn
+    case $yn in
+        [Yy]* ) install_vpn; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo '###### Done setup! ######'
