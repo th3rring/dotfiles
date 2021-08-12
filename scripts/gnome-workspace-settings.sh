aaf6c918746e619c0a412f46bd264efe25f8973f
Thomas Herring <teh6@rice.edu>
2021-08-12 18:49:14 -0500

    Increased to 8 workspaces

--- scripts/gnome-workspace-settings.sh
+++ scripts/gnome-workspace-settings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-num_workspaces=6
+num_workspaces=8
 
 gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 8
 
 gsettings set org.gnome.shell.keybindings switch-to-application-1  []
 gsettings set org.gnome.shell.keybindings switch-to-application-2  []
@@ -11,8 +11,8 @@ gsettings set org.gnome.shell.keybindings switch-to-application-3  []
 gsettings set org.gnome.shell.keybindings switch-to-application-4  []
 gsettings set org.gnome.shell.keybindings switch-to-application-5  []
 gsettings set org.gnome.shell.keybindings switch-to-application-6  []
-# gsettings set org.gnome.shell.keybindings switch-to-application-7  []
-# gsettings set org.gnome.shell.keybindings switch-to-application-8  []
+gsettings set org.gnome.shell.keybindings switch-to-application-7  []
+gsettings set org.gnome.shell.keybindings switch-to-application-8  []
 # gsettings set org.gnome.shell.keybindings switch-to-application-9  []
 
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1  "['<Super>1']"
@@ -21,8 +21,8 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3  "['<Super>
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4  "['<Super>4']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5  "['<Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6  "['<Super>6']"
-# gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7  "['<Super>7']"
-# gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8  "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7  "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8  "['<Super>8']"
 # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9  "['<Super>9']"
 # gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
 
@@ -32,7 +32,7 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3  "['<Super><S
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4  "['<Super><Shift>4']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5  "['<Super><Shift>5']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6  "['<Super><Shift>6']"
-# gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7  "['<Super><Shift>7']"
-# gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8  "['<Super><Shift>8']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7  "['<Super><Shift>7']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8  "['<Super><Shift>8']"
 # gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9  "['<Super><Shift>9']"
 # gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']
