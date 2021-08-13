d65674a56a1676ceed62445849ea6567076bce24
Thomas Herring <teh6@rice.edu>
2021-08-13 11:44:55 -0500

    Changed to new builder images

--- scripts/builderconnect-launch.sh
+++ scripts/builderconnect-launch.sh
@@ -5,4 +5,4 @@ docker run \
     -p 10000:10000 \
     --user $(id -u):$(id -g) \
     -v $KL_BUILD_WORKSPACE_PATH:/Workspace \
-    th3rring/builder:dart-xpra bash 
+    th3rring/builder:xpra bash 
